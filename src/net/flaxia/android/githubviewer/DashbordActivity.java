041d4fc391ef2a61a692cbb3cc49e2752dbc8eca
fgluser <vine.de.wine@gmail.com>
2011-03-08 01:11:40 +0900

    検索ボタンを作った

--- src/net/flaxia/android/githubviewer/DashbordActivity.java
+++ src/net/flaxia/android/githubviewer/DashbordActivity.java
@@ -1,12 +1,34 @@
 package net.flaxia.android.githubviewer;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DashbordActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
+    }
+    
+    /**
+     * 検索ボタンを押したときの処理
+     * @param view
+     */
+    public void onSearchButton(View view){
+        Context context = getApplicationContext();
+        String q = ((EditText)findViewById(R.id.q)).getText().toString();
+        //TODO:検索ワードがブランクのみだった場合も弾く
+        if(0 == q.length()){
+            Toast.makeText(context, R.string.search_word_is_empty, Toast.LENGTH_LONG).show();
+            return;
+        }
+        Intent intent = new Intent(context, SearchActivity.class);
+        intent.putExtra(SearchActivity.Q, q);
+        startActivity(intent);
     }
 }
---
+++ src/net/flaxia/android/githubviewer/SearchActivity.java
@@ -0,0 +1,7 @@
+package net.flaxia.android.githubviewer;
+
+import android.app.Activity;
+
+public class SearchActivity extends Activity {
+    public static final String Q = "q";
+}
