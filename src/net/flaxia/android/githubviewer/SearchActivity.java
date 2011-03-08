715583e8c5c1f054ac7241badff677047932088b
fgluser <vine.de.wine@gmail.com>
2011-03-08 14:46:53 +0900

    検索を実行できるようにした

--- src/net/flaxia/android/githubviewer/SearchActivity.java
+++ src/net/flaxia/android/githubviewer/SearchActivity.java
@@ -1,7 +1,21 @@
 package net.flaxia.android.githubviewer;
 
+import org.idlesoft.libraries.ghapi.GitHubAPI;
+import org.idlesoft.libraries.ghapi.APIAbstract.Response;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SearchActivity extends Activity {
     public static final String Q = "q";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashbord);
+
+        GitHubAPI github = new GitHubAPI();
+        github.goStealth();
+        Response res = github.repo.search((String) getIntent().getExtras().get(Q));
+        System.out.println(res.resp.toString());
+    }
 }
