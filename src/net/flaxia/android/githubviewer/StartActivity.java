ff3826651f7d243a6af8b5c98c14ea55c85dd0dd
fgluser <vine.de.wine@gmail.com>
2011-03-11 21:17:53 +0900

    AndroidManifestに応じて，Configのdebugを変更するようにした

--- src/net/flaxia/android/githubviewer/StartActivity.java
+++ src/net/flaxia/android/githubviewer/StartActivity.java
@@ -2,12 +2,24 @@ package net.flaxia.android.githubviewer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 
 public class StartActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().isDebuggable = isDebuggable();
         startActivity(new Intent(getApplicationContext(), DashbordActivity.class));
+    }
+
+    private boolean isDebuggable() {
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), 0);
+            return ((ai.flags & ApplicationInfo.FLAG_DEBUGGABLE) == ApplicationInfo.FLAG_DEBUGGABLE);
+        } catch (NameNotFoundException e) {
+            return false;
+        }
     }
 }
