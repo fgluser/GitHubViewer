1e4fe7485868ae2d9c095065b49af87aa518ad81
fgluser <vine.de.wine@gmail.com>
2011-03-12 17:36:54 +0900

    メッセージにint型を直接指定できるようにした

--- src/net/flaxia/android/githubviewer/Log.java
+++ src/net/flaxia/android/githubviewer/Log.java
@@ -6,11 +6,19 @@ public class Log {
             android.util.Log.d(tag, msg);
         }
     }
+    
+    public static void d(String tag, int msg) {
+        d(tag, String.valueOf(msg));
+    }
 
     public static void e(String tag, String msg) {
         if (Configuration.getInstance().isDebuggable) {
             android.util.Log.e(tag, msg);
         }
+    }
+    
+    public static void e(String tag, int msg) {
+        e(tag, String.valueOf(msg));
     }
 
     public static void i(String tag, String msg) {
@@ -18,16 +26,28 @@ public class Log {
             android.util.Log.i(tag, msg);
         }
     }
+    
+    public static void i(String tag, int msg) {
+        i(tag, String.valueOf(msg));
+    }
 
     public static void v(String tag, String msg) {
         if (Configuration.getInstance().isDebuggable) {
             android.util.Log.v(tag, msg);
         }
     }
+    
+    public static void v(String tag, int msg) {
+        v(tag, String.valueOf(msg));
+    }
 
     public static void w(String tag, String msg) {
         if (Configuration.getInstance().isDebuggable) {
             android.util.Log.w(tag, msg);
         }
+    }
+    
+    public static void w(String tag, int msg) {
+        w(tag, String.valueOf(msg));
     }
 }
