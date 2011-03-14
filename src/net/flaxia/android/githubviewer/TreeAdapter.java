6d51bc7d5ffa408a4018eb2ace22018622c91e4b
fgluser <vine.de.wine@gmail.com>
2011-03-14 21:26:04 +0900

    リファクタリング

--- src/net/flaxia/android/githubviewer/TreeAdapter.java
+++ src/net/flaxia/android/githubviewer/TreeAdapter.java
@@ -2,9 +2,7 @@ package net.flaxia.android.githubviewer;
 
 import java.util.ArrayList;
 
-import net.flaxia.android.githubviewer.util.CommonHelper;
 import net.flaxia.android.githubviewer.util.IconCache;
-
 import android.content.Context;
 import android.text.Html;
 import android.view.View;
@@ -30,48 +28,38 @@ public class TreeAdapter extends KeyValuePairAdapter {
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         View view = super.getDropDownView(position, convertView, parent);
         int level = Integer.parseInt(mKeyValuePairs.get(position).getValue());
-        if (level == 0) {
-            ((TextView) view.findViewById(android.R.id.text1)).setText(mKeyValuePairs.get(position)
-                    .getKey());
-        } else {
-            String html = "";
-            boolean flag = false;
-            int i = position + 1;
-            while (i < mKeyValuePairs.size()) {
-                int value = Integer.parseInt(mKeyValuePairs.get(i).getValue());
-                if (value == level) {
-                    html = "<img src=\"dir_t\">";
-                    flag = true;
-                    break;
-                }
-                if (value < level) {
-                    break;
-                }
-                i++;
+        String html = "";
+        boolean flag = true;
+        for (int i = position + 1, size = mKeyValuePairs.size(); i < size; i++) {
+            int value = Integer.parseInt(mKeyValuePairs.get(i).getValue());
+            if (value == level) {
+                html = "<img src=\"dir_t\">";
+                flag = false;
+                break;
+            } else if (value < level) {
+                break;
             }
-            if (!flag) {
-                html = "<img src=\"dir_l\">";
-            }
-            int downLevel = level - 1;
-            while (0 != downLevel) {
-                int k = position + 1;
-                while (k < mKeyValuePairs.size()) {
-                    int value = Integer.parseInt(mKeyValuePairs.get(k).getValue());
-                    if (value == downLevel) {
-                        html = "<img src=\"dir_i\">" + html;
-                        break;
-                    }
-                    if (value < downLevel) {
-                        html = "<img src=\"dir_blank\">" + html;
-                        break;
-                    }
-                    k++;
-                }
-                downLevel--;
-            }
-            ((TextView) view.findViewById(android.R.id.text1)).setText(Html.fromHtml(html
-                    + mKeyValuePairs.get(position).getKey(), IconCache.getInstance(), null));
         }
+        if (flag && 0 != level) {
+            html = "<img src=\"dir_l\">";
+        }
+
+        for (int i = level - 1; 0 < i; i--) {
+            for (int j = position + 1, size = mKeyValuePairs.size(); j < size; j++) {
+                int value = Integer.parseInt(mKeyValuePairs.get(j).getValue());
+                if (value == i) {
+                    html = "<img src=\"dir_i\">" + html;
+                    break;
+                }
+                if (value < i) {
+                    html = "<img src=\"dir_blank\">" + html;
+                    break;
+                }
+            }
+        }
+        ((TextView) view.findViewById(android.R.id.text1)).setText(Html.fromHtml(html
+                + mKeyValuePairs.get(position).getKey(), IconCache.getInstance(), null));
+
         return view;
     }
 }
