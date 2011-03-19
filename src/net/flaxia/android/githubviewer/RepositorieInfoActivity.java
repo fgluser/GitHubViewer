f84d8b03e58359cb1085c700ed640ae69e8acfc2
fgluser <vine.de.wine@gmail.com>
2011-03-19 22:01:50 +0900

    RepositorieInfoActivityを整理

--- src/net/flaxia/android/githubviewer/RepositorieInfoActivity.java
+++ src/net/flaxia/android/githubviewer/RepositorieInfoActivity.java
@@ -11,8 +11,9 @@ public class RepositorieInfoActivity extends BaseAsyncActivity {
         setContentView(R.layout.activity_repositorie_info);
         Repositorie repositorie = (Repositorie) getIntent().getExtras().getSerializable(
                 Extra.REPOSITORIE);
-        ((TextView) findViewById(R.id.repositorie_name)).setText(repositorie.get(Repositorie.NAME));
-        ((TextView) findViewById(R.id.owner)).setText(repositorie.get(Repositorie.OWNER));
+        ((TextView) findViewById(R.id.owner_repositorie)).setText(repositorie
+                .get(Repositorie.OWNER)
+                + " / " + repositorie.get(Repositorie.NAME));
         ((TextView) findViewById(R.id.homepage)).setText(repositorie.get(Repositorie.HOMEPAGE));
         ((TextView) findViewById(R.id.description)).setText(repositorie
                 .get(Repositorie.DESCRIPTION));
