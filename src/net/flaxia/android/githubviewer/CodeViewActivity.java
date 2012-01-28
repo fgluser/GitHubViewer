bdc2712e9cf935d68c64fab517e955f14c584370
fgluser <vine.de.wine@gmail.com>
2012-01-29 03:05:23 +0900

    CodeViewActivityをAsyncTaskLoaderで書き直した

--- src/net/flaxia/android/githubviewer/CodeViewActivity.java
+++ src/net/flaxia/android/githubviewer/CodeViewActivity.java
@@ -4,19 +4,27 @@ package net.flaxia.android.githubviewer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.flaxia.android.githubviewer.util.CommonHelper;
 import net.flaxia.android.githubviewer.util.Extra;
-import net.flaxia.android.githubviewer.util.LogEx;
 
 import org.idlesoft.libraries.ghapi.GitHubAPI;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,11 +32,10 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class CodeViewActivity extends BaseAsyncActivity {
-    private static final String TAG = CodeViewActivity.class.getSimpleName();
+public class CodeViewActivity extends FragmentActivity implements LoaderCallbacks<String> {
     private static final int MENU_COPY = Menu.FIRST + 1;
     protected WebView mWebView;
-    private ProgressDialog mRenderingDialog;
+    private ProgressDialog mProgressDialog;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -37,7 +44,7 @@ public class CodeViewActivity extends BaseAsyncActivity {
         initWebView();
         final String url = getIntent().getStringExtra(Intent.EXTRA_TITLE);
         if (null == url || 0 == url.length()) {
-            doAsyncTask();
+            getSupportLoaderManager().initLoader(0, null, this);
         } else {
             mWebView.loadUrl(url);
         }
@@ -54,7 +61,7 @@ public class CodeViewActivity extends BaseAsyncActivity {
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(final WebView view, final String url, final Bitmap favicon) {
-                mRenderingDialog = ProgressDialog.show(CodeViewActivity.this, null,
+                mProgressDialog = ProgressDialog.show(CodeViewActivity.this, null,
                         getString(R.string.rendering), true, true);
                 super.onPageStarted(view, url, favicon);
             }
@@ -62,91 +69,13 @@ public class CodeViewActivity extends BaseAsyncActivity {
             @Override
             public void onPageFinished(final WebView view, final String url) {
                 super.onPageFinished(view, url);
-                if (null != mRenderingDialog && mRenderingDialog.isShowing()) {
-                    mRenderingDialog.dismiss();
+                if (null != mProgressDialog && mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
                 }
                 mWebView.clearCache(true);
 
             }
         });
-    }
-
-    @Override
-    protected void executeAsyncTask(final String... parameters) {
-        final String html = createHtml();
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                mProgressDialog.dismiss();
-                mWebView.loadDataWithBaseURL("about:blank", html, "text/html", "utf-8", null);
-            }
-        });
-    }
-
-    /**
-     * 表示するHTMLを生成する
-     * 
-     * @return
-     */
-    private String createHtml() {
-        String html;
-        final Bundle extras = getIntent().getExtras();
-        final String path = extras.getString(Extra.EXPLORER_PATH);
-        String source;
-        if (null == path || 0 == path.length()) {
-            source = new GitHubAPI().object.raw(extras.getString("owner"),
-                    extras.getString("name"), extras.getString("sha")).resp;
-        } else {
-            try {
-                final BufferedReader br = new BufferedReader(new FileReader(path));
-                final StringBuilder sb = new StringBuilder();
-                String str;
-                while ((str = br.readLine()) != null) {
-                    sb.append(str + "\n");
-                }
-                source = sb.toString();
-            } catch (final IOException e) {
-                e.printStackTrace();
-                source = "";
-            }
-        }
-        final StringBuilder sb = new StringBuilder();
-        try {
-            BufferedReader br = null;
-            try {
-                br = new BufferedReader(new InputStreamReader(getResources().getAssets().open(
-                        "html")));
-                for (String str = br.readLine(); null != str; str = br.readLine()) {
-                    sb.append(str + "\n");
-                }
-            } finally {
-                if (null != br) {
-                    br.close();
-                }
-            }
-            html = sb.toString();
-        } catch (final IOException e) {
-            e.printStackTrace();
-            html = null;
-        }
-        try {
-            if (null == source || null == html) {
-                html = getResources().getString(R.string.failed_to_retrieve_the_information);
-            } else {
-                html = html.replaceFirst("@title", extras.getString("fileName"));
-                html = html.replaceFirst("@source", CommonHelper.escapeSign(source));
-                final String theme = PreferenceManager.getDefaultSharedPreferences(
-                        getApplicationContext()).getString(ConfigureActivity.CODE_THEME, "default");
-                html = html.replaceFirst("@theme", theme);
-                LogEx.d(TAG, theme);
-                LogEx.d(TAG, source);
-            }
-        } catch (final Exception e) {
-            e.printStackTrace();
-            html = "";
-        }
-
-        return html;
     }
 
     @Override
@@ -174,4 +103,89 @@ public class CodeViewActivity extends BaseAsyncActivity {
         keyEvent.dispatch(mWebView);
     }
 
+    @Override
+    public Loader<String> onCreateLoader(int arg0, Bundle arg1) {
+        mProgressDialog = ProgressDialog.show(this, null, getString(R.string.now_loading), true,
+                true, new OnCancelListener() {
+                    @Override
+                    public void onCancel(final DialogInterface dialog) {
+                        getSupportLoaderManager().destroyLoader(0);
+                        finish();
+                    }
+                });
+        final AsyncTaskLoader<String> asyncTaskLoader = new AsyncTaskLoader<String>(
+                getBaseContext()) {
+            private String getSource() throws IOException {
+                final Bundle extras = getIntent().getExtras();
+                final String path = extras.getString(Extra.EXPLORER_PATH);
+                if (null == path || 0 == path.length()) {
+                    return new GitHubAPI().object.raw(extras.getString("owner"),
+                            extras.getString("name"), extras.getString("sha")).resp;
+                }
+                final StringBuilder sb = new StringBuilder();
+                final BufferedReader br = new BufferedReader(new FileReader(path));
+                while (br.ready()) {
+                    sb.append(br.readLine() + "\n");
+                }
+                br.close();
+
+                return sb.toString();
+            }
+
+            private String getHtml() throws IOException {
+                final InputStream inputStream = getResources().getAssets().open("html");
+                final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                final StringBuilder stringBuilder = new StringBuilder();
+                while (bufferedReader.ready()) {
+                    stringBuilder.append(bufferedReader.readLine() + "\n");
+                }
+                return stringBuilder.toString();
+            }
+
+            private String replace(final String source, final Map<String, String> map) {
+                if (map.isEmpty()) {
+                    return source;
+                }
+                final String key = map.keySet().iterator().next();
+                final String value = map.remove(key);
+
+                return replace(source.replaceFirst(key, value), map);
+            }
+
+            @Override
+            public String loadInBackground() {
+                final Bundle extras = getIntent().getExtras();
+                try {
+                    final String source = getSource();
+                    final String html = getHtml();
+
+                    final Map<String, String> map = new HashMap<String, String>();
+                    map.put("@title", extras.getString("fileName"));
+                    map.put("@source", CommonHelper.escapeSign(source));
+                    map.put("@theme",
+                            PreferenceManager.getDefaultSharedPreferences(getBaseContext())
+                                    .getString(ConfigureActivity.CODE_THEME, "default"));
+
+                    return replace(html, map);
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+                return getString(R.string.failed_to_retrieve_the_information);
+            }
+        };
+        asyncTaskLoader.forceLoad();
+
+        return asyncTaskLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> arg0, String arg1) {
+        mProgressDialog.dismiss();
+        mWebView.loadDataWithBaseURL("about:blank", arg1, "text/html", "utf-8", null);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> arg0) {
+    }
 }
